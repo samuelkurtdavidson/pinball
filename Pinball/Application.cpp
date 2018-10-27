9735a654041139005f8abfea8f163f0c366c8a05
sherzock <jacobo.galofre@gmail.com>
2018-10-27 14:18:16 +0200

    Module Fonts bug fix

--- Pinball/Application.cpp
+++ Pinball/Application.cpp
@@ -8,6 +8,7 @@
 #include "ModulePlayer.h"
 #include "ModulePhysics.h"
 #include "ModuleSceneIntro.h"
+#include "ModuleFonts.h"
 
 #include "Application.h"
 
@@ -21,6 +22,7 @@ Application::Application()
 	player = new ModulePlayer(this);
 	scene_intro = new ModuleSceneIntro(this);
 	physics = new ModulePhysics(this);
+	fonts = new ModuleFonts(this);
 
 	// The order of calls is very important!
 	// Modules will Init() Start() and Update in this order
@@ -34,6 +36,7 @@ Application::Application()
 	AddModule(input);
 	AddModule(audio);
 	
+	AddModule(fonts);
 	// Scenes
 	AddModule(scene_intro);
 	
