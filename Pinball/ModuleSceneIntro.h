283a64e55e08d94fc350393ef01974bf9504b327
sherzock <jacobo.galofre@gmail.com>
2018-10-28 10:49:12 +0100

    font.png and ModuleScene update

--- Pinball/ModuleSceneIntro.h
+++ Pinball/ModuleSceneIntro.h
@@ -18,12 +18,32 @@ public:
 	void OnCollision(PhysBody* bodyA, PhysBody* bodyB);
 
 public:
-	p2List<PhysBody*> circles;
+	/*p2List<PhysBody*> circles;
 	p2List<PhysBody*> boxes;
 	p2List<PhysBody*> ricks;
 
 	SDL_Texture* circle;
 	SDL_Texture* box;
-	SDL_Texture* rick;
+	SDL_Texture* rick;*/
 	uint bonus_fx;
+	PhysBody* maps;
+	p2List<PhysBody*> bouncers;
+	p2List<PhysBody*> sensors;
+
+	PhysBody* leftflipper;
+	PhysBody* rightflipper;
+
+	bool dead = false;
+	bool sensed;
+	int font_score = -1;
+	char score_text[10];
+
+private:
+
+	void PinballGround();
+	void setScores();
+	void AddBouncers();
+	void UpdateSensors();
+	void PlayerLives();
+	void SceneChange();
 };
